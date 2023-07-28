1f960ad5b8e636b7d794e659dcc200068e5f3d37
Yazdan <116789371+yazdan-rm@users.noreply.github.com>
2023-07-28 19:22:23 +0330

    CommitFirst

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", (req, res)=>{
+    
+    res.sendFile(__dirname+ "/signup.html");
+});
+
+
+let firstName, lastName, email;
+
+app.post("/", (req, res)=>{
+    
+    firstName = req.body.fName;
+    lastName = req.body.lName;
+    email = req.body.email;
+
+
+    const data = {
+        members:[
+            {
+                email_address: email,
+                status: "subscribed", 
+                merge_fields: {
+                    FNAME:firstName,
+                    LNAME:lastName
+                }
+            }
+        ]
+    };
+    
+    const jsonData = JSON.stringify(data);
+    
+    url = "https://us10.api.mailchimp.com/3.0/lists/05b435eaf8";
+    
+    const option = {
+        method:"POST",
+        auth: "yazdan1:f013f44cdb6e4876e9e13d7e565494b8-us10"
+    }
+    
+    const request = https.request(url, option, (response)=>{
+    
+        response.on("data", (data)=>{
+            console.log(JSON.parse(data));
+        })
+
+        console.log(response.statusCode);
+        if(response.statusCode === 200) res.sendFile(__dirname + "/success.html");
+
+        res.sendFile(__dirname + "/failure.html");
+        
+    });
+    
+    request.write(jsonData);
+    request.end();
+});
+
+
+app.post("/failure", (req, res)=>{
+    
+    res.redirect("/");
+})
+
+
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log(`Server is running on port ${port}.`);
+})
+
+//API key   
+// f013f44cdb6e4876e9e13d7e565494b8-us10
+
+
+//List ID
+//05b435eaf8
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,36 @@
+    // const data = {
+    //     members:[
+    //         {
+    //             email_address: email,
+    //             status: "subscribed", 
+    //             merge_fields: {
+    //                 FNAME:firstName,
+    //                 LNAME:lastName
+    //             }
+    //         }
+    //     ]
+    // };
+    
+    // const jsonData = JSON.stringify(data);
+    
+    // url = "https://us10.api.mailchimp.com/3.0/lists/05b435eaf8";
+    
+    // const option = {
+    //     method:"POST",
+    //     auth: "yazdan1:849a52e4885fdfa39ee3df4c62ea0ab6-us10"
+    // }
+    
+    // const request = https.request(url, option, (response)=>{
+    
+    //     response.on("data", (data)=>{
+    //         console.log(JSON.parse(data));
+    //     })
+
+    //     if(response.statusCode === 200) res.sendFile(__dirname + "/success.html");
+
+    //     res.sendFile(__dirname + "/failure.html");
+
+    // });
+    
+    // request.write(jsonData);
+    // request.end();
