2d5c1a43799aad4d192afeccc6de6ca0b9a06547
Yazdan <1yazdanrafieemanesh@gmail.com>
2023-09-29 11:29:16 +0330

    new update

--- app.js
+++ app.js
@@ -6,78 +6,66 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.get("/", (req, res)=>{
-    
-    res.sendFile(__dirname+ "/signup.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
 });
-
 
 let firstName, lastName, email;
 
-app.post("/", (req, res)=>{
-    
-    firstName = req.body.fName;
-    lastName = req.body.lName;
-    email = req.body.email;
+app.post("/", (req, res) => {
+  firstName = req.body.fName;
+  lastName = req.body.lName;
+  email = req.body.email;
 
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
 
-    const data = {
-        members:[
-            {
-                email_address: email,
-                status: "subscribed", 
-                merge_fields: {
-                    FNAME:firstName,
-                    LNAME:lastName
-                }
-            }
-        ]
-    };
-    
-    const jsonData = JSON.stringify(data);
-    
-    url = "https://us10.api.mailchimp.com/3.0/lists/05b435eaf8";
-    
-    const option = {
-        method:"POST",
-        auth: "yazdan1:5f7148df955758f18acbc32fb4304a69-us10"
-    }
-    
-    const request = https.request(url, option, (response)=>{
-    
-        response.on("data", (data)=>{
-            console.log(JSON.parse(data));
-        })
+  const jsonData = JSON.stringify(data);
 
-        console.log(response.statusCode);
-        if(response.statusCode === 200) res.sendFile(__dirname + "/success.html");
+  url = "https://us10.api.mailchimp.com/3.0/lists/05b435eaf8";
 
-        res.sendFile(__dirname + "/failure.html");
-        
+  const option = {
+    method: "POST",
+    auth: "d9cf843c9a7e7f894f5680c7bcd27819-us10",
+  };
+
+  const request = https.request(url, option, (response) => {
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
     });
-    
-    request.write(jsonData);
-    request.end();
+
+    console.log(response.statusCode);
+    if (response.statusCode === 200) res.sendFile(__dirname + "/success.html");
+
+    res.sendFile(__dirname + "/failure.html");
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
+app.post("/failure", (req, res) => {
+  res.redirect("/");
+});
 
-app.post("/failure", (req, res)=>{
-    
-    res.redirect("/");
-})
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
 
-
-
-app.listen(process.env.PORT || port, ()=>{
-    console.log(`Server is running on port ${port}.`);
-})
-
-//API key   
+//API key
 // 5f7148df955758f18acbc32fb4304a69-us10
-
 
 //List ID
 //05b435eaf8
